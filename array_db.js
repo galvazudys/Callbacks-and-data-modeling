d3b878557a8471fae55d5c11c7e7833f977d3a4d
galvazudys <golfas705@yahoo.com>
2017-09-27 18:41:01 +0100

    created db's

---
+++ array_db.js
@@ -0,0 +1,290 @@
+module.exports = [
+  {
+    id: "87be3db8-fc75-4996-aa49-8f94ed5f8d9a",
+    user: { name: "Estell Shanahan", userName: "Tyreek_Little" },
+    email: "Lisandro40@gmail.com",
+    age: 51663,
+    location: "Andorra",
+    hobies: "input",
+    image: "http://lorempixel.com/640/480"
+  },
+  {
+    id: "b997f6a1-6bc1-40c2-8340-2f31fa6f0ad3",
+    user: { name: "Alivia Reinger", userName: "Bert_Haley17" },
+    email: "Zechariah_Walker16@hotmail.com",
+    age: 45399,
+    location: "Uganda",
+    hobies: "parse",
+    image: "http://lorempixel.com/640/480"
+  },
+  {
+    id: "d2dca503-35c5-4c95-bcc6-0ee9960a557e",
+    user: { name: "Vincenza Jacobs", userName: "Irving81" },
+    email: "Gloria.Beier@gmail.com",
+    age: 63034,
+    location: "Philippines",
+    hobies: "navigate",
+    image: "http://lorempixel.com/640/480"
+  },
+  {
+    id: "e8dc7821-afe2-4f85-8b7f-235de99b1725",
+    user: { name: "Alexandrea Rohan", userName: "Quinten58" },
+    email: "Marco96@gmail.com",
+    age: 74478,
+    location: "Venezuela",
+    hobies: "reboot",
+    image: "http://lorempixel.com/640/480"
+  },
+  {
+    id: "d65f8dae-bdbf-4f03-937d-d6452cb447e6",
+    user: { name: "Barrett Moore", userName: "Providenci_Kassulke99" },
+    email: "Freda16@yahoo.com",
+    age: 63913,
+    location: "Romania",
+    hobies: "program",
+    image: "http://lorempixel.com/640/480"
+  },
+  {
+    id: "e662d2c9-d5af-4d40-95f8-2eaa2c39824e",
+    user: { name: "Dudley Bartell", userName: "Fidel75" },
+    email: "Mollie13@yahoo.com",
+    age: 58433,
+    location: "Uganda",
+    hobies: "copy",
+    image: "http://lorempixel.com/640/480"
+  },
+  {
+    id: "e703b7d8-9fe3-4c22-995b-a56d8c126193",
+    user: { name: "Omer Heller", userName: "Krista.Hoppe56" },
+    email: "Brain.Hackett93@gmail.com",
+    age: 63777,
+    location: "Luxembourg",
+    hobies: "connect",
+    image: "http://lorempixel.com/640/480"
+  },
+  {
+    id: "465eb50f-31b4-467e-a437-6b01b1757f36",
+    user: { name: "Tod Paucek", userName: "Delphia10" },
+    email: "Neil56@gmail.com",
+    age: 58609,
+    location: "China",
+    hobies: "override",
+    image: "http://lorempixel.com/640/480"
+  },
+  {
+    id: "155e8aab-876e-48c8-809e-9323f97b7e8d",
+    user: { name: "Celine Swaniawski", userName: "Kelton.Mosciski" },
+    email: "Niko_Olson35@hotmail.com",
+    age: 56065,
+    location: "Barbados",
+    hobies: "copy",
+    image: "http://lorempixel.com/640/480"
+  },
+  {
+    id: "722bb490-1137-40a0-b813-e9d914be0ed4",
+    user: { name: "Lesley Schmidt", userName: "Ellen_Fadel" },
+    email: "Felipa_Adams45@yahoo.com",
+    age: 28423,
+    location: "Swaziland",
+    hobies: "bypass",
+    image: "http://lorempixel.com/640/480"
+  },
+  {
+    id: "ae289a4d-192a-4331-bd33-5d9fb86d3298",
+    user: { name: "Gardner Leuschke", userName: "Arvid28" },
+    email: "Jany.Jenkins38@yahoo.com",
+    age: 88612,
+    location: "Guernsey",
+    hobies: "parse",
+    image: "http://lorempixel.com/640/480"
+  },
+  {
+    id: "916cdf3a-5f0b-4395-bcf3-bf860c66acae",
+    user: { name: "Rylan Beatty", userName: "Letha0" },
+    email: "Liza_Kohler63@gmail.com",
+    age: 12682,
+    location: "South Africa",
+    hobies: "connect",
+    image: "http://lorempixel.com/640/480"
+  },
+  {
+    id: "b520fcb6-9379-40b5-b62b-a001c12a6907",
+    user: { name: "Emilie Halvorson MD", userName: "Gonzalo_Bruen9" },
+    email: "Leonor.Bergstrom91@hotmail.com",
+    age: 21115,
+    location: "South Georgia and the South Sandwich Islands",
+    hobies: "generate",
+    image: "http://lorempixel.com/640/480"
+  },
+  {
+    id: "5633a052-0197-4a4c-b5ec-9afb8f953eb3",
+    user: { name: "Kiera Abshire Sr.", userName: "Ferne80" },
+    email: "Hubert.Wehner@hotmail.com",
+    age: 9437,
+    location: "Maldives",
+    hobies: "program",
+    image: "http://lorempixel.com/640/480"
+  },
+  {
+    id: "27466160-51f0-4a3a-9b1c-12ffbe7e8633",
+    user: { name: "Blanche White I", userName: "Trent.Zemlak" },
+    email: "Wilhelm.Hagenes13@gmail.com",
+    age: 31009,
+    location: "Central African Republic",
+    hobies: "synthesize",
+    image: "http://lorempixel.com/640/480"
+  },
+  {
+    id: "bf8fcbc0-08a4-47d2-bdc6-9adec55d2b4f",
+    user: { name: "Kane Renner", userName: "Kale.Schaefer47" },
+    email: "Lexie.Harris35@yahoo.com",
+    age: 84312,
+    location: "Falkland Islands (Malvinas)",
+    hobies: "compress",
+    image: "http://lorempixel.com/640/480"
+  },
+  {
+    id: "18cf530c-3386-49df-ba56-790bc239455b",
+    user: { name: "Jaida Hintz", userName: "Markus_Graham" },
+    email: "Marjorie_Gutkowski@gmail.com",
+    age: 35940,
+    location: "Denmark",
+    hobies: "calculate",
+    image: "http://lorempixel.com/640/480"
+  },
+  {
+    id: "19951b8c-2be0-4f2e-8cfe-8ebae2718f85",
+    user: { name: "Albin MacGyver", userName: "Dameon66" },
+    email: "Kattie_Stokes@yahoo.com",
+    age: 10612,
+    location: "Dominican Republic",
+    hobies: "compress",
+    image: "http://lorempixel.com/640/480"
+  },
+  {
+    id: "4a52d412-e902-4c45-b8e8-7d192d835440",
+    user: { name: "Lauriane Gislason Sr.", userName: "Grayson21" },
+    email: "Chance.Schumm@hotmail.com",
+    age: 40381,
+    location: "Palau",
+    hobies: "connect",
+    image: "http://lorempixel.com/640/480"
+  },
+  {
+    id: "297e103d-7351-44a7-9a3e-fc7a6e4ed4c6",
+    user: { name: "Darion Ebert", userName: "Carmine27" },
+    email: "Keyshawn.Schroeder62@hotmail.com",
+    age: 65023,
+    location: "Cyprus",
+    hobies: "transmit",
+    image: "http://lorempixel.com/640/480"
+  },
+  {
+    id: "9ae43d71-fa1f-4202-8a7f-da36bc416439",
+    user: { name: "Kenya Schuster", userName: "Elyssa99" },
+    email: "Ally_MacGyver@yahoo.com",
+    age: 53687,
+    location: "Netherlands",
+    hobies: "hack",
+    image: "http://lorempixel.com/640/480"
+  },
+  {
+    id: "522bfb76-00f6-424b-b7c4-0924d23724e6",
+    user: { name: "Dianna MacGyver", userName: "Eldora_VonRueden34" },
+    email: "Justus32@hotmail.com",
+    age: 69105,
+    location: "Canada",
+    hobies: "calculate",
+    image: "http://lorempixel.com/640/480"
+  },
+  {
+    id: "250b9220-d19d-419d-8ad6-ff533c29e7a0",
+    user: { name: "Jack Bailey", userName: "Albina.Kub" },
+    email: "Ezra_Beier21@yahoo.com",
+    age: 2056,
+    location: "Tajikistan",
+    hobies: "navigate",
+    image: "http://lorempixel.com/640/480"
+  },
+  {
+    id: "88b34a23-a8e8-4a45-9ee5-875c22a7da99",
+    user: { name: "Fabiola Windler", userName: "Alia.McClure" },
+    email: "Raymond18@hotmail.com",
+    age: 70116,
+    location: "Virgin Islands, British",
+    hobies: "reboot",
+    image: "http://lorempixel.com/640/480"
+  },
+  {
+    id: "563f122b-acf8-4dd5-9537-0d651f9e161e",
+    user: { name: "Rico Conroy", userName: "Ramiro_Schroeder" },
+    email: "Brianne_Ledner@gmail.com",
+    age: 8296,
+    location: "Barbados",
+    hobies: "connect",
+    image: "http://lorempixel.com/640/480"
+  },
+  {
+    id: "a570d6fc-2134-409f-829c-8d3aa585bdf4",
+    user: { name: "Nicklaus Murazik", userName: "Gennaro.Kuhn54" },
+    email: "Gretchen_Rosenbaum@hotmail.com",
+    age: 24625,
+    location: "Uruguay",
+    hobies: "program",
+    image: "http://lorempixel.com/640/480"
+  },
+  {
+    id: "8892523c-7115-4070-bfe5-0099da62aef9",
+    user: { name: "Lesly Bailey", userName: "Toni.Crooks" },
+    email: "Merle.Ortiz29@hotmail.com",
+    age: 8850,
+    location: "Venezuela",
+    hobies: "bypass",
+    image: "http://lorempixel.com/640/480"
+  },
+  {
+    id: "68b73cee-2e4b-436d-b9c4-f76e0334d8c1",
+    user: { name: "Gunnar Mayer DVM", userName: "Alisha38" },
+    email: "Kimberly_Schulist95@yahoo.com",
+    age: 47403,
+    location: "Reunion",
+    hobies: "transmit",
+    image: "http://lorempixel.com/640/480"
+  },
+  {
+    id: "3417f3f2-3c28-4a74-838b-a7aef07b590d",
+    user: { name: "Naomi Reichert", userName: "Aliya10" },
+    email: "Emery_OHara@hotmail.com",
+    age: 37608,
+    location: "Iraq",
+    hobies: "parse",
+    image: "http://lorempixel.com/640/480"
+  },
+  {
+    id: "71850931-f34f-4a98-8e16-e272bf440271",
+    user: { name: "Tyrese Ward", userName: "Penelope_Lemke" },
+    email: "Marion_McKenzie29@hotmail.com",
+    age: 51364,
+    location: "Nauru",
+    hobies: "reboot",
+    image: "http://lorempixel.com/640/480"
+  },
+  {
+    id: "fa3ebd76-2c19-4735-8f13-3dffbc85e352",
+    user: { name: "Jonas Schinner", userName: "Jacques.Schultz39" },
+    email: "Bill_Gislason@yahoo.com",
+    age: 37978,
+    location: "Serbia",
+    hobies: "override",
+    image: "http://lorempixel.com/640/480"
+  },
+  {
+    id: "6ef8ca0e-a31e-4fe0-94fb-4fdc60330412",
+    user: { name: "Dr. Jeanette Murazik", userName: "Josie.Collier" },
+    email: "Austen9@hotmail.com",
+    age: 8454,
+    location: "Russian Federation",
+    hobies: "parse",
+    image: "http://lorempixel.com/640/480"
+  }
+];
--- model.js
+++ model.js
@@ -1,27 +1,22 @@
-const faker = require('faker');
+const faker = require("faker");
+const db = require("./array_db");
+const objDb = require("./objects_db");
+// const fs = require('fs');
+// const newobj={};
+// db.forEach((item,index)=>{
+//   newobj[item.id] = item;
+// })
+
+// console.log(objDb);
+
+// fs.writeFile('./objects_db.js', JSON.stringify(newobj, null, 2) , 'utf-8',()=>{console.log('success')})
+
+// outputs: "Marks, Dean Sr."
 
 var data_model_array = {
-  db: [
-    { name: 'Mr. Josefa Wyman', id: 'ee9d6b8d-4aa5-4f1e-853e-c1e0783bb68d' },
-    { name: 'Hillary Hettinger', id: 'fa58fbff-5b95-4a1a-8d71-097191a3a532' },
-    { name: 'Conrad Effertz', id: '7260faba-c3e8-45b8-add8-1dc0d1b65fac' },
-    { name: 'Grayce Dietrich', id: 'ac871240-931e-4283-b792-17c49c3930ab' },
-    { name: 'Henriette Cummerata', id: 'c0bbd2ec-9fef-40b6-abab-69902011f169' },
-    { name: 'Angelo Reichert', id: '233763ec-bd43-48fb-a27b-baa1e60ed065' },
-    { name: 'Murphy Rosenbaum', id: '519e717b-38aa-4f19-b54c-66d0e4964277' },
-    { name: 'Dee Olson', id: '476d59ae-0712-4d47-9a74-0f0d3df00822' },
-    { name: 'Chance Denesik', id: '9377822d-08fa-4d6b-aadf-e7c9ff1b556b' },
-    { name: 'Ellen Smith Sr.', id: '30d5cde0-5677-4317-8af8-68c06d790bcb' },
-    { name: 'Madge Pagac', id: '405cc310-76eb-4f84-849b-fa22f7974834' },
-    { name: 'Kiley Schiller', id: 'e7bd2bf3-c61d-4aa5-838e-d19b92d7cf8d' },
-    { name: 'Madaline Bartoletti', id: '268bc8a1-a38a-4125-9da0-9cc5a8cf9d94' },
-    { name: 'Franz Hartmann', id: 'dacbb149-bfaa-46c4-8448-ccba8b6f97f9' },
-    { name: 'Jennifer Renner', id: 'dc1d20c0-7f0d-4708-8b7d-5f78e007ee16' },
-    { name: 'Felton Schumm', id: 'fc4a9067-ecbb-42fe-9b4d-4ccfe1f62902' },
-    { name: 'Dennis Hintz', id: '14245ac2-d216-4710-9eb1-7e7ec5700333' }
-  ],
+  db: db,
 
-  create(new_object) {
+  create(new_object, callBack) {
     let id = faker.random.uuid();
     if (
       !this.db.find(x => {
@@ -30,7 +25,9 @@ var data_model_array = {
         }
       })
     ) {
-      this.db.push({ name: new_object, id: id });
+      const weHaveData = this.db.push({ name: new_object, id: id });
+      console.log(weHaveData);
+      callBack({ message: "success" });
     } else {
       while (
         db.find(x => {
@@ -43,6 +40,7 @@ var data_model_array = {
         return id;
       }
       this.db.push({ name: new_object, id: id });
+      callBack({ message: "success" });
     }
   },
   read(entry_id) {
@@ -62,7 +60,10 @@ var data_model_array = {
     const userIndex = this.db.find((user, index) => {
       return user.id === entry_id ? index : null;
     });
-    this.db.splice(userIndex, 1, { name: new_value, id: entry_id });
+    this.db.splice(userIndex, 1, {
+      name: new_value,
+      id: entry_id
+    });
   },
   remove(entry_id) {
     const userIndex = this.db.find((user, index) => {
@@ -73,25 +74,7 @@ var data_model_array = {
 };
 
 var data_model_object = {
-  db: {
-    'ee9d6b8d-4aa5-4f1e-853e-c1e0783bb68d': { name: 'Mr. Josefa Wyman' },
-    'fa58fbff-5b95-4a1a-8d71-097191a3a532': { name: 'Hillary Hettinger' },
-    '7260faba-c3e8-45b8-add8-1dc0d1b65fac': { name: 'Conrad Effertz' },
-    'ac871240-931e-4283-b792-17c49c3930ab': { name: 'Grayce Dietrich' },
-    'c0bbd2ec-9fef-40b6-abab-69902011f169': { name: 'Henriette Cummerata' },
-    '233763ec-bd43-48fb-a27b-baa1e60ed065': { name: 'Angelo Reichert' },
-    '519e717b-38aa-4f19-b54c-66d0e4964277': { name: 'Murphy Rosenbaum' },
-    '476d59ae-0712-4d47-9a74-0f0d3df00822': { name: 'Dee Olson' },
-    '9377822d-08fa-4d6b-aadf-e7c9ff1b556b': { name: 'Chance Denesik' },
-    '30d5cde0-5677-4317-8af8-68c06d790bcb': { name: 'Ellen Smith Sr.' },
-    '405cc310-76eb-4f84-849b-fa22f7974834': { name: 'Madge Pagac' },
-    'e7bd2bf3-c61d-4aa5-838e-d19b92d7cf8d': { name: 'Kiley Schiller' },
-    '268bc8a1-a38a-4125-9da0-9cc5a8cf9d94': { name: 'Madaline Bartoletti' },
-    'dacbb149-bfaa-46c4-8448-ccba8b6f97f9': { name: 'Franz Hartmann' },
-    'dc1d20c0-7f0d-4708-8b7d-5f78e007ee16': { name: 'Jennifer Renner' },
-    'fc4a9067-ecbb-42fe-9b4d-4ccfe1f62902': { name: 'Felton Schumm' },
-    '14245ac2-d216-4710-9eb1-7e7ec5700333': { name: 'Dennis Hintz' }
-  },
+  db: objDb,
 
   create(new_Object) {
     let id = faker.random.uuid();
@@ -101,34 +84,40 @@ var data_model_object = {
         return id;
       }
     }
-    this.db[id] = { name: new_Object };
+    this.db[id] = {
+      name: new_Object
+    };
   },
-  read(entry_id){
-      const promise = new Promise((resolve,reject)=>{
-          const data = this.db[entry_id];
-          if(data){
-              resolve(data);
-          }else{
-              reject();
-          }
-      })
+  read(entry_id) {
+    const promise = new Promise((resolve, reject) => {
+      const data = this.db[entry_id];
+      if (data) {
+        resolve(data);
+      } else {
+        reject();
+      }
+    });
 
-      return promise;
+    return promise;
   },
-  update(entry_id,newValue){
-    if(this.db[entry_id]){
-        this.db[entry_id] = {name:newValue}
-    }else{
-        throw new Error('this user do not exist');
+  update(entry_id, newValue) {
+    if (this.db[entry_id]) {
+      this.db[entry_id] = {
+        name: newValue
+      };
+    } else {
+      throw new Error("this user do not exist");
     }
   },
-  remove(entry_id){
-      if(this.db[entry_id]){
-          delete this.db[entry_id];
-      }else{
-        throw new Error('this user do not exist');        
-      }
+  remove(entry_id) {
+    if (this.db[entry_id]) {
+      delete this.db[entry_id];
+    } else {
+      throw new Error("this user do not exist");
+    }
   }
 };
 
 
+
+data_model_array.read("87be3db8-fc75-4996-aa49-8f94ed5f8d9a").then((d)=>{console.log(d)});
